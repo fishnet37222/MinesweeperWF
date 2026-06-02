dc7e722c92975a453a432b520fa634d0545b427d
David Frischknecht <fishnet37222@outlook.com>
2026-06-01 20:06:08 -0400

    Add persistent JSON-based application settings
    Implemented loading and saving of application settings using a JSON file stored in the user's AppData directory. Settings are loaded at startup and saved on application exit. Exposed a static method to access the settings node.

--- MinesweeperWF/Program.cs
+++ MinesweeperWF/Program.cs
@@ -2,14 +2,41 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+using System.Text.Json.Nodes;
+
 namespace MinesweeperWF;
 
 public static class Program
 {
+	private static JsonNode? s_settingsNode;
+	private static string? s_settingsFilePath;
+
 	[STAThread]
 	public static void Main()
 	{
 		ApplicationConfiguration.Initialize();
+
+		s_settingsFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "FishNetSoft", "MinesweeperWF", "settings.json");
+
+		if (File.Exists(s_settingsFilePath))
+		{
+			s_settingsNode = JsonNode.Parse(File.ReadAllText(s_settingsFilePath));
+		}
+		else
+		{
+			s_settingsNode = new JsonObject();
+		}
+
+		Application.ApplicationExit += Application_ApplicationExit;
+
 		Application.Run(new MainWindow());
 	}
+
+	private static void Application_ApplicationExit(object? sender, EventArgs e)
+	{
+		Directory.CreateDirectory(Path.GetDirectoryName(s_settingsFilePath)!);
+		File.WriteAllText(s_settingsFilePath!, s_settingsNode!.ToJsonString());
+	}
+
+	public static JsonNode GetSettingsNode() => s_settingsNode!;
 }
